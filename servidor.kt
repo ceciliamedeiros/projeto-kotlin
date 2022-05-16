8139fbe1922c24ca868bc822f6243736c004b7cc
lcsm-ecomp <32273415+lcsm-ecomp@users.noreply.github.com>
2022-05-16 11:59:12 -0300

    Atualização da aula dia 14/05

--- servidor.kt
+++ servidor.kt
@@ -22,18 +22,72 @@ fun geraTextoOla(nome:String, empresa:String) = """
              </HTML>
            """.trimIndent()
 
+class Usuario(val nome:String, val telefone:String, val tipo:String)
+
+val usuarios:MutableList<Usuario> = mutableListOf()
+
+
+
+fun criaListaUsuarios() : String = usuarios.joinToString(separator="\n")  {u -> 
+    "<br>Nome: ${u.nome}, Telefone: ${u.telefone}, Tipo: ${u.tipo}"
+}
 
 fun main() {
    println("iniciando servidor...")
    embeddedServer(Netty, port = 7654) {
       routing {
+         get("/lista_usuarios") {
+            call.respondText("""
+            <html>
+            <body>
+            <CENTER><H1>Lista de Usuarios Cadastrados</H1></CENTER><p>
+            ${criaListaUsuarios()}
+            </body>
+            </html>
+            """,ContentType.Text.Html)
+         
+         }
+         post("/cria_novo_usuario") {
+            val parameters = call.receiveParameters() // se post
+            //val parameters = call.parameters // se get
+            
+            val nome = parameters["nome_usuario"] ?: "desc"
+            val telefone = parameters["telefone_usuario"] ?: "0"
+            val tipo = parameters["tipo_usuario"] ?: "aluno"
+            usuarios.add(Usuario(nome,telefone,tipo))
+            call.respondText("""
+            <html>
+            <body>
+            Usuario Cadastrado na lista do servidor<p>
+            Nome: ${nome}<p>
+            Telefone: ${telefone}<p>
+            Tipo: ${tipo}<p>
+            </body>
+            </html>
+            """,ContentType.Text.Html)
+         }
+         get("/soma/{x}/{y}") {
+            val x = (call.parameters["x"] as String).toInt()
+            val y = (call.parameters["y"] as String).toInt()
+            call.respondText("${x+y}")
+         }
+         get("/bom_dia.html") {
+            println("recebi um chamado de bom dia")
+            call.respondText("""
+            <html>
+            <body>
+            Bom dia, aqui é o servidor <b>HTTP</b>
+            </body>
+            </html>
+            """,ContentType.Text.Html)
+         }
          get("/ola/{nome}/{empresa}") {
            val nome = call.parameters["nome"] as String
            val empresa = call.parameters["empresa"] as String
            println("O servidor recebeu uma solicitacao de $nome")
            call.respondText(geraTextoOla(nome, empresa),ContentType.Text.Html)
          }
-         static("/estatico/") {
+         static("/") {
            files("static/")
          }   
       }
